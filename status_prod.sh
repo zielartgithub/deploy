7d3c1a1219a5a52804a2d2df178ef9319d5d882b
azl <artur.zielinski@sollers.eu>
2019-03-28 21:26:11 +0100

    Add status monitoring

---
+++ status_prod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+while ! `curl -s http://ec2-18-185-117-59.eu-central-1.compute.amazonaws.com:8081/status | grep -q '"status": "ok"'`; do echo "${PIPESTATUS[0]}" && sleep 10; done;
+        exit 0
